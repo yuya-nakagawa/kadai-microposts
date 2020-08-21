fb92f64c238fb12b11b6c9494febda0485fd2245
yuya.nakagawa <yuye4049@gmail.com>
2020-08-21 10:19:11 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :create]
+  
+  resources :microposts, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
